f08669c8a06dbfecdd1bc78a710fd58e839a2af8
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-06-12 15:11:01 +0900

    초기 멤버 데이터 추가

--- FileUploadProject/src/main/resources/data.sql
+++ FileUploadProject/src/main/resources/data.sql
@@ -1,1 +1,7 @@
-insert into member (id, user_name, password) values (1000, 'tae77777@naver.com', 1234);
+insert into member (id, user_name, password)
+values (1000, 'tae77777@naver.com', 1234);
+insert into member (id, user_name, password)
+values (1001, '김태헌', 1234);
+insert into member (id, user_name, password)
+values (1002, '나혜령', 1234);
+
